f401d3b26f634e990c033fa16dda392bcbaeab6f
Leontev-Aleksej <leontevaleksej485@gmail.com>
2025-04-10 03:41:19 +0300

    make migrations

---
+++ database/migrations/2026_04_10_004008_create_reports_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('fullname');
+            $table->string('path_img');
+            $table->string('theme');
+            $table->foreignId('section_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
